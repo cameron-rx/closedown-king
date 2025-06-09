1d33de5d8407b8233686d17c21a5a85928c3e054
Cameron Robinson <camjr246@yahoo.co.uk>
2025-06-09 18:36:32 +0100

    feat(server): Scheduled deletion of all non-complete checklists every morning at 7am

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Checklist;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    $deleted = Checklist::where('is_complete', false)->delete();
+})->dailyAt('07:00');
