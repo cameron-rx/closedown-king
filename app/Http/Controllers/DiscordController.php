5e8d5eeddb96f37c04e53fa8d0709f376b9b4214
Cameron Robinson <camjr246@yahoo.co.uk>
2025-06-10 20:55:47 +0100

    feat(discord): Automatic whitelisting and admin role based on server roles

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class DiscordController extends Controller
 {
@@ -25,6 +26,44 @@ class DiscordController extends Controller
 
     ]);
 
+    // TODO: Auto verification and role assignment
+    // Get user access token 
+    // Make http request to guilds endpoint 
+    // if 404 verified = false
+    // else verified = true
+    // check roles for tl/manager id 
+    // if  present admin = true
+    // else admin = false
+    // Revs guild id = 963448945592373320
+
+    $accessToken = $discordUser->token;
+
+    $response = Http::withToken($accessToken)->get('https://discord.com/api/v10/users/@me/guilds/963448945592373320/member');
+
+
+    if ($response->ok()) {
+        // role ids for managment and team leader role in server
+        $adminRoles = ['963496000935301181', '971022818055696384'];
+        // server specific nickname
+        $nickname = $response['nick'];
+        $roles = $response['roles'];
+        $admin = false;
+
+        foreach ($adminRoles as $roleId) {
+            if (in_array($roleId, $roles)) {
+                $admin = true;
+                break;
+            }
+        } 
+
+        $user->name = $nickname;
+        $user->is_whitelisted = true;
+        $user->is_admin = $admin;
+        $user->save();
+    }
+
+
+
 
 
     Auth::login($user);
@@ -33,7 +72,8 @@ class DiscordController extends Controller
 
     public function redirect()
     {
-        return Socialite::driver('discord')->redirect();
+        return Socialite::driver('discord')->scopes(['guilds.members.read'])->redirect();
+
     }
 
 }
