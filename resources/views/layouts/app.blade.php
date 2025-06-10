743e9fb1f6b3bca78acee85c0fb0179b78d699fc
Cameron Robinson <camjr246@yahoo.co.uk>
2025-06-10 13:44:58 +0100

    feat(favicons): Added images for icon use across devices

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <script src="//unpkg.com/alpinejs" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
 </head>
 
 <body class="bg-background min-h-dvh flex flex-col">
