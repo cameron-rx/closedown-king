674d8a72d0c8b77f4deefa640d8a444d9e732811
Cameron Robinson <camjr246@yahoo.co.uk>
2025-06-19 18:32:02 +0100

    feat(checklists): Added styling for checklist create and edit on desktop

--- resources/views/checklist-create.blade.php
+++ resources/views/checklist-create.blade.php
@@ -3,13 +3,13 @@
 
 @section('content')
 <div class="flex flex-col gap-8 p-8 justify-around grow items-center">
-    <h1 class="text-8xl text-center font-semibold">Choose a Checklist</h1>
-    <div class="flex flex-col grow justify-start w-full">
+    <h1 class="text-8xl lg:text-5xl text-center font-semibold">Choose a Checklist</h1>
+    <div class="flex flex-col grow justify-start w-full lg:w-1/3">
         @foreach ($templates as $template)
         <form class="flex flex-row " method="post" action="/checklist">
             @csrf
             <input type="hidden" name="template_id" value="{{ $template->id }}">
-            <button class="flex flex-row fill items-center justify-center grow bg-custom-blue text-white font-semibold shadow-custom-grey shadow-md rounded-3xl p-5 m-5 text-5xl hover:cursor-pointer" type="submit">{{ $template->name }}</button>
+            <button class="flex flex-row fill items-center justify-center grow bg-custom-blue text-white font-semibold shadow-custom-grey shadow-md rounded-3xl p-5 m-5 text-5xl lg:text-3xl hover:cursor-pointer" type="submit">{{ $template->name }}</button>
         </form>
         @endforeach
     </div>
--- resources/views/checklist-edit.blade.php
+++ resources/views/checklist-edit.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 
-<div class="flex flex-col grow justify-around p-8">
-        <h1 class="font-bold text-center text-7xl">{{ $checklist->name}}</h1>
+<div class="flex flex-col grow justify-around p-8 lg:ml-144 lg:mr-144">
+        <h1 class="font-bold text-center text-7xl lg:text-5xl">{{ $checklist->name}}</h1>
 
-        <form class="flex flex-col justify-between grow gap-24" method="post" action="{{ route('checklists.update', $checklist->id) }}">
+        <form class="flex flex-col justify-between grow gap-24 " method="post" action="{{ route('checklists.update', $checklist->id) }}">
             @csrf
             @method('PATCH')
 
                 @foreach ($checklist->sections()->get() as $section)
-                <div class="flex flex-col grow gap-8 pl-8 pr-8">
-                    <h2 class="text-7xl text-center font-semibold p-8">{{$section->name}}</h2>
+                <div class="flex flex-col grow gap-8 lg:gap-4 pl-8 pr-8">
+                    <h2 class="text-7xl lg:text-4xl text-center font-semibold p-8">{{$section->name}}</h2>
                     @foreach ($section->items()->get() as $item)
                     <div class="flex flex-row items-center justify-between gap-4">
                         <input type="checkbox" name="{{ $item->id }}" id="{{ $item->id }}"
-                            class="text-custom-green w-16 h-16 peer" />
+                            class="text-custom-green w-16 h-16 lg:w-8 lg:h-8 peer" />
                         <label for="{{ $item->id }}"
-                            class="text-5xl peer-checked:line-through peer-checked:opacity-40 order-first">{{$item->name}}</label>
+                            class="text-5xl lg:text-2xl peer-checked:line-through peer-checked:opacity-40 order-first">{{$item->name}}</label>
                     </div>
                     @endforeach
                 </div>
